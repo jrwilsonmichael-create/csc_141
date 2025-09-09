b81e3610359931bec5a6eb5c08a214f594ddfe88
Mikeythe1 <jrwilsonmichael@gmail.com>
2025-09-09 11:50:04 -0400

    Add files via upload

---
+++ assignments / 02_variables_and_data_types/02_11_adding_comments.py
@@ -0,0 +1,4 @@
+# I will be adding comments to my code to explain what each line does
+print("hello world this is my third week of doing python and I'm sort of having fun")
+
+# I will write a comment above each line of code to explain what it does
