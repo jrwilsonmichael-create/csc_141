d1750a178872f0177058bfc391b9f6d74b18c11d
Mikeythe1 <jrwilsonmichael@gmail.com>
2025-09-09 12:18:13 -0400

    Update 05_01_conditional_tests.py

--- assignments /05_if_statements/05_01_conditional_tests.py
+++ assignments /05_if_statements/05_01_conditional_tests.py
@@ -1,1 +1,5 @@
+# I will add conditional tests and print a statement describing each test and its result
 
+car = 'Mustang'
+print(car == 'Mustang')  # True, because car is 'Mustang'
+print(car == 'Camaro')   # False, because car is not 'Camaro'
