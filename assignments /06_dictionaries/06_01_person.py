68cb9cd93ebab1a38d7cb5bc44eaf83bb9f44c8d
Mikeythe1 <jrwilsonmichael@gmail.com>
2025-09-11 12:06:28 -0400

    Update 06_01_person.py

--- assignments /06_dictionaries/06_01_person.py
+++ assignments /06_dictionaries/06_01_person.py
@@ -1,1 +1,6 @@
-
+# I will use a dictionary to store information about a person
+person = {'first_name': 'Michael','last_name': 'Wilson', 'age': 18, 'city': 'Philadelphia'}
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
