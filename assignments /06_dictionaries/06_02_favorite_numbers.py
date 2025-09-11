8838c228f3d25a5e3bb8fc505ccde0b1be1d9ece
Mikeythe1 <jrwilsonmichael@gmail.com>
2025-09-11 12:07:11 -0400

    Add files via upload

---
+++ assignments /06_dictionaries/06_02_favorite_numbers.py
@@ -0,0 +1,5 @@
+# I will use a dictionary to store people's favorite numbers
+favorite_numbers = {'Michael A': 8, 'Michael W': 1, 'Mr.Kaul': 6}
+print(favorite_numbers['Michael A'])
+print(favorite_numbers['Michael W'])
+print(favorite_numbers['Mr.Kaul'])
