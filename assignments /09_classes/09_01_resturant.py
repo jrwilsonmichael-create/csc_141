e755f0254fd6e56c6d393c5f4fb4ccdc504128be
Mikeythe1 <jrwilsonmichael@gmail.com>
2025-09-11 12:23:06 -0400

    Create 09_01_resturant.py

---
+++ assignments /09_classes/09_01_resturant.py
@@ -0,0 +1,10 @@
+# I will make a class called Resturant
+class Resturant:
+    def __init__(self):
+        pass
+    def display_resturant(self):
+        print("The resturant is called The 1's Palace.")
+        def open_resturant(self):
+            print("The resturant is open.")
+            def set_cuisine_Type(self):
+                print("The cuisine type is American.")
