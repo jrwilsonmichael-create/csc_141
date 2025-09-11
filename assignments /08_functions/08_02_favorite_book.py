6a81336bfe9933ca0ebaa040e3af9c591a5c3fa4
Mikeythe1 <jrwilsonmichael@gmail.com>
2025-09-11 12:11:54 -0400

    Add files via upload

---
+++ assignments /08_functions/08_02_favorite_book.py
@@ -0,0 +1,3 @@
+# I will write a function called favorite_book that accepts one parameter, title.
+def favorite_book(The_Great_Gatsby):
+    print("One of my favorite books is " + The_Great_Gatsby+".")
