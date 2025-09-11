5f694c9872295d1e8e8f0c757fd23c6362c7a7b2
Mikeythe1 <jrwilsonmichael@gmail.com>
2025-09-11 12:09:22 -0400

    Create 07_01_rental_car.py

---
+++ assignments /07_user_input_and_while_loops/07_01_rental_car.py
@@ -0,0 +1,3 @@
+# I will write a program that asks the user what kind of rental car they would like.
+car = input("What kind of rental car would you like? ")
+print("Let me see if I can find you a " + car + ".")
