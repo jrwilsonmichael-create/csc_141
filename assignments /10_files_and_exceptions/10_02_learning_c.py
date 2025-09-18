6d334c37fe408cff007bb7ab559669c237f58a67
Mikeythe1 <jrwilsonmichael@gmail.com>
2025-09-18 12:43:01 -0400

    Add files via upload

---
+++ assignments /10_files_and_exceptions/10_02_learning_c.py
@@ -0,0 +1,4 @@
+# I will be learning C programming and will write a few lines about it in a text file.
+message = "I really like burgers and fries"
+message.replace('burgers', 'fries')
+'I really like burgers.'
