c1238909d1088af5184d279d1b4d72307c1358c3
Mikeythe1 <jrwilsonmichael@gmail.com>
2025-12-11 12:54:18 -0500

    Delete assignments directory

--- assignments/12_22_scoring.py
+++
@@ -1,7 +0,0 @@
-# I will make a scoring system
-def calculate_score(enemies_destroyed, bonus):
-    score = enemies_destroyed * 100 + bonus
-    print(f"Your total score is: {score}")
-    return score
-# Call the calculate_score function to test it
-calculate_score(15, 500)
